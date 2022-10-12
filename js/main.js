63ef99406718eea9a76357d0609a26302d88c758
Daria Mey <d.like.jagger@gmail.com>
2022-10-12 14:35:45 +0300

    Исправляет функции

--- js/main.js
+++ js/main.js
@@ -5,19 +5,22 @@ function getRandomNumber(min, max) {
   if (typeof min !== 'number' || typeof max !== 'number') {
     return NaN;
   }
+  if ( min < 0 || max < 0) {
+    return NaN;
+  }
+  if (min >= max) {
+    return NaN;
+  }
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
 getRandomNumber(20,140);
 
+
 //Функция для проверки максимальной длины строки
 
 function checkStringLength(string, maxLength) {
-  if (string.length <= maxLength) {
-    return true;
-  } else {
-    return false;
-  }
+  return string.length <= maxLength;
 }
 
 checkStringLength('fvfv',140);
