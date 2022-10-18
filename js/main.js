3f9a5b1cc8a86d0344889d6fb3fc3319b15741d8
Daria Mey <d.like.jagger@gmail.com>
2022-10-18 12:24:26 +0300

    Создание массива

--- js/main.js
+++ js/main.js
@@ -24,3 +24,56 @@ function checkStringLength(string, maxLength) {
 }
 
 checkStringLength('fvfv',140);
+
+
+//Генерация объектов
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+const PHOTO_DESCRIPTION_COUNT = 25;
+
+const IDS = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25
+];
+
+const URL_PHOTOS = [
+  'photos/1.jpg',
+  'photos/2.jpg',
+  'photos/3.jpg',
+  'photos/4.jpg',
+  'photos/5.jpg',
+  'photos/6.jpg',
+  'photos/7.jpg',
+  'photos/8.jpg',
+  'photos/9.jpg',
+  'photos/10.jpg',
+  'photos/11.jpg',
+  'photos/12.jpg',
+  'photos/13.jpg',
+  'photos/14.jpg',
+  'photos/15.jpg',
+  'photos/16.jpg',
+  'photos/17.jpg',
+  'photos/18.jpg',
+  'photos/19.jpg',
+  'photos/20.jpg',
+  'photos/21.jpg',
+  'photos/22.jpg',
+  'photos/23.jpg',
+  'photos/24.jpg',
+  'photos/25.jpg',
+];
+
+const DESCRIPTIONS = [
+  'О', 'П','И','С','А','Н','И','Е'
+];
+
+const getPhotoDiscription = () => ({
+  id: getRandomArrayElement(IDS),
+  url: getRandomArrayElement(URL_PHOTOS),
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomNumber(15, 200),
+  comments: getRandomNumber(0, 200),
+});
+
+const photoDescription = Array.from({length: PHOTO_DESCRIPTION_COUNT}, getPhotoDiscription);
