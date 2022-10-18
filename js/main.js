4895ecb9804438f30c55db2ad797bf28afaaf948
Daria Mey <d.like.jagger@gmail.com>
2022-10-18 14:05:22 +0300

    Исправления

--- js/main.js
+++ js/main.js
@@ -31,49 +31,24 @@ checkStringLength('fvfv',140);
 const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
 
 const PHOTO_DESCRIPTION_COUNT = 25;
+const ID_COUNT = 25;
+const URL_COUNT = 25;
+const LIKES_COUNT = 200;
+const COMMENTS_COUNT = 200;
 
-const IDS = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25
-];
-
-const URL_PHOTOS = [
-  'photos/1.jpg',
-  'photos/2.jpg',
-  'photos/3.jpg',
-  'photos/4.jpg',
-  'photos/5.jpg',
-  'photos/6.jpg',
-  'photos/7.jpg',
-  'photos/8.jpg',
-  'photos/9.jpg',
-  'photos/10.jpg',
-  'photos/11.jpg',
-  'photos/12.jpg',
-  'photos/13.jpg',
-  'photos/14.jpg',
-  'photos/15.jpg',
-  'photos/16.jpg',
-  'photos/17.jpg',
-  'photos/18.jpg',
-  'photos/19.jpg',
-  'photos/20.jpg',
-  'photos/21.jpg',
-  'photos/22.jpg',
-  'photos/23.jpg',
-  'photos/24.jpg',
-  'photos/25.jpg',
-];
 
 const DESCRIPTIONS = [
-  'О', 'П','И','С','А','Н','И','Е'
+  'О','П','И','С','А','Н','И','Е'
 ];
 
 const getPhotoDiscription = () => ({
-  id: getRandomArrayElement(IDS),
-  url: getRandomArrayElement(URL_PHOTOS),
+  id: getRandomNumber(1, ID_COUNT),
+  url: `photos/${getRandomNumber(1, URL_COUNT)}.jpg`,
   description: getRandomArrayElement(DESCRIPTIONS),
-  likes: getRandomNumber(15, 200),
-  comments: getRandomNumber(0, 200),
+  likes: getRandomNumber(15, LIKES_COUNT),
+  comments: getRandomNumber(0, COMMENTS_COUNT),
 });
 
-const photoDescription = Array.from({length: PHOTO_DESCRIPTION_COUNT}, getPhotoDiscription);
+const photoDescription = () => Array.from({length: PHOTO_DESCRIPTION_COUNT}, getPhotoDiscription);
+
+photoDescription();
