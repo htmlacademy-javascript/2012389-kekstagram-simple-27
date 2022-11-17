840a274b02b2e3450dbbaf25028a02ce2fec91e7
Daria Mey <d.like.jagger@gmail.com>
2022-11-17 13:28:11 +0300

    Удалила лишнее

--- js/util.js
+++ js/util.js
@@ -18,14 +18,6 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-//Функция для проверки максимальной длины строки
-
-function checkStringLength(string, maxLength) {
-  return string.length <= maxLength;
-}
-
-// checkStringLength(1,140);
-
 //Функция для получения случайного элемента массива
 
 const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
